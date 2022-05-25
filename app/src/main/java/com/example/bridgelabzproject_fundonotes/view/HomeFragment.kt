c122aaf7a882c6e0f09f633fed718f716ce4d607
Niket-K-Bhaskar <bniket19@gmail.com>
2022-05-25 15:00:00 +0530

    Label functionality working

--- app/src/main/java/com/example/bridgelabzproject_fundonotes/view/HomeFragment.kt
+++ app/src/main/java/com/example/bridgelabzproject_fundonotes/view/HomeFragment.kt
@@ -63,6 +63,7 @@ class HomeFragment : Fragment() {
         binding.addNoteBtn.setOnClickListener(){
             Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_fragmentAddNote)
         }
+
         var highPriorityArray = ArrayList<UserNote>()
         var mediumPriorityArray = ArrayList<UserNote>()
         var lowPriorityArray = ArrayList<UserNote>()
